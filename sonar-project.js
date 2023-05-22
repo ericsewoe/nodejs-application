a87b6b1b24a8ffc7949f7ddf925cf1ffb1413aca
ericsewoe <128120784+ericsewoe@users.noreply.github.com>
2023-05-22 09:57:31 +0000

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://52.87.175.79:9000/',
+  serverUrl: 'http://54.174.233.153:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsJomacsit',
-	    'sonar.login': '927ddd32ebfc93a8a14cfe6d9d6b4272e2d1c55b',
+	    'sonar.login': '3160713a46a3acb6bf258a4c946adfd27034efae',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
